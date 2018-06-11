e1a7fc48d72364768f6b3c9f4e5686287cbd4931
gcardoso89 <goncalo.cardoso@safetyculture.io>
2018-06-12 09:08:25 +1000

    Adds new texts and deletes a few questions

--- finals.js
+++ finals.js
@@ -1,29 +1,29 @@
 module.exports = {
 	"bambi": {
-		title: "Bambi",
-		subtitle: "La jeune pousse",
+		title: "BAMBI",
+		subtitle: "la jeune pousse",
 		description: "Tu débutes dans le métier mais tu commences à te frotter à la réalité de ce boulot. Encore plein d’enthousiasme et de dynamisme, tu as raison ! Ne perds pas la foi, ce métier est passionnant et te permettra de sonder les profondeurs de l’âme humaine et de réaliser de belles choses pour l’intérêt général.",
 		facebook: "Bambi, La jeune pousse, Toi aussi viens découvrir quel collaborateur ou collaboratrice de cabinet tu es ! Un test original de l'Asso Dircab #40ans #Dircab #Quizz",
 		twitter: "Bambi, La jeune pousse, Toi aussi viens découvrir quel collaborateur ou collaboratrice de cabinet tu es ! Un test original de @AssoDircab #40ans #Dircab #Quizz"
 	},
 	"charles-ingalls": {
-		title: "Charles Ingalls",
-		subtitle: "Le bon père de famille",
-		description: "Tu en as déjà vu pas mal ! On peut compter sur toi et ton expérience. Tu sais rester zen, quelles que soient les demandes et sollicitations. Mais attention, tu n’as pas encore atteint le niveau suprême du dircab chevronné(e). Que la force soit avec toi !",
+		title: "CHARLES INGALLS",
+		subtitle: "le bon père de famille",
+		description: "Tu en as déjà vu pas mal ! On peut compter sur toi et ton expérience. Tu sais rester zen, quelles que soient les demandes et sollicitations. Mais attention, tu n’as pas encore atteint le niveau suprême du DirCab chevronné(e). Que la force soit avec toi !",
 		facebook: "Charles Ingalls, Le bon père de famille, Toi aussi viens découvrir quel collaborateur ou collaboratrice de cabinet tu es ! Un test original de l'Asso Dircab #40ans #Dircab #Quizz",
 		twitter: "Charles Ingalls, Le bon père de famille, Toi aussi viens découvrir quel collaborateur ou collaboratrice de cabinet tu es ! Un test original de @AssoDircab #40ans #Dircab #Quizz"
 	},
 	"mac-gyver": {
-		title: "Mac Gyver",
-		subtitle: "L’astucieux, le malin",
-		description: "Les situations inextricables te sont familières et cette expérience fait de toi un véritable MacGyver des cabinets. Ton arme à toi, ce n’est pas le couteau suisse mais une maxime bien française : même dans la bouse, le coq chante toujours. Si tu as surmonté ces situations, aucune inquiétude pour ton avenir.",
+		title: "MAC GYVER",
+		subtitle: "l’astucieux, le malin",
+		description: "Gyver des cabinets. Ton arme à toi, ce n’est pas le couteau suisse mais une maxime bien française : « Même dans la bouse, le coq chante toujours ». Si tu as surmonté ces situations, aucune inquiétude pour ton avenir.",
 		facebook: "Mac Gyver, L’astucieux, le malin, Toi aussi viens découvrir quel collaborateur ou collaboratrice de cabinet tu es ! Un test original de l'Asso Dircab #40ans #Dircab #Quizz",
 		twitter: "Mac Gyver, L’astucieux, le malin, Toi aussi viens découvrir quel collaborateur ou collaboratrice de cabinet tu es ! Un test original de @AssoDircab #40ans #Dircab #Quizz"
 	},
 	"maitre-yoda": {
-		title: "MAITRE YODA",
-		subtitle: "Le sage, l’état suprême",
-		description: "Tu es THE dircab chevronné(e) ! Tu as tout vu, tout entendu. Rien ne peut plus t’étonner. Tu peux maintenant consacrer le (peu de) temps qu’il te reste à former la nouvelle génération en t’impliquant dans l’association DirCab !",
+		title: "MAÎTRE YODA",
+		subtitle: "le sage, l’état suprême",
+		description: "Tu es THE DirCab chevronné(e) ! Tu as tout vu, tout entendu. Rien ne peut plus t’étonner. Tu peux maintenant consacrer le (peu de) temps qu’il te reste à former la nouvelle génération en t’impliquant dans l’association DirCab !",
 		facebook: "MAITRE YODA, Le sage, l’état suprême, Toi aussi viens découvrir quel collaborateur ou collaboratrice de cabinet tu es ! Un test original de l'Asso Dircab #40ans #Dircab #Quizz",
 		twitter: "MAITRE YODA, Le sage, l’état suprême, Toi aussi viens découvrir quel collaborateur ou collaboratrice de cabinet tu es ! Un test original de @AssoDircab #40ans #Dircab #Quizz"
 	}
